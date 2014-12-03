b76644dc8fa586f3867effac7e653ba193406597
taddeimania <jtaddei@gmail.com>
2014-12-03 09:29:54 -0600

    Version: bumped to 1.6.2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-multiselect",
-    version="1.6.1",
+    version="1.6.2",
     description="Django multiselect",
     author="Matt Morrison and Aaron Madison",
     include_package_data=True,
     zip_safe=False,
     packages=find_packages(exclude=['example*']),
-    install_requires = open('requirements/dist.txt').read().split('\n'),
+    install_requires=open('requirements/dist.txt').read().split('\n'),
 )
